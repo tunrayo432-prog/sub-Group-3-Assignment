c6b04f2f6f87bab9115fb3440055a1b4bed09c2e
Amos Kabba Kamara <amoskabbakamaradev@gmail.com>
2026-03-01 22:20:53 +0000

    Sub-Group-3 Assignment completed

---
+++ Express.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
+
+// Users API
+const users = [
+    {id: 1, date:'1/03/2026', category: 'Text Book', amounts: 3000},
+    {id: 2, date: '1/03/2026', categry: 'Shoes', amounts: 1500}
+];
+
+
+
+app.listen(port, () => {console.log(`This app is listing to port ${port}`)});
