fd8742ebd13c240e5eadbcb9608a1e7da96507a4
Amos Kabba Kamara <amoskabbakamaradev@gmail.com>
2026-03-03 20:31:51 +0000

    Name: Amos K  Kamara

--- Express.js
+++ Express.js
@@ -4,8 +4,8 @@ const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 
-// Users API
-const users = [
+//  API that allows users to add, update and delete expenses
+const expenses = [
     {id: 1, date:'1/03/2026', category: 'Text Book', amounts: 3000},
     {id: 2, date: '1/03/2026', categry: 'Shoes', amounts: 1500}
 ];
