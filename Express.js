b1e5d4cfb0e264dd044c2e945ef58ef1c5a33f82
ayoade motunrayo queeneth <tunrayo432@gmail.com>
2026-03-06 04:03:44 +1300

    Added GET all endpoint - Ayoade motunrayo queeneth

--- Express.js
+++ Express.js
@@ -1,17 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
 
-//  API that allows users to add, update and delete expenses
+// API that allows users to add, update and delete expenses
 const expenses = [
-    {id: 1, date:'1/03/2026', category: 'Text Book', amounts: 3000},
-    {id: 2, date: '1/03/2026', categry: 'Shoes', amounts: 1500}
+    { id: 1, date: '1/03/2026', category: 'Text Book', amounts: 3000 },
+    { id: 2, date: '1/03/2026', category: 'Shoes', amounts: 1500 }
 ];
 
-// Get all - Read
+// GET ALL - Read Expenses
+app.get('/expenses', (req, res) => {
+    res.json(expenses);
+});
+
+// Start Server (MUST BE LAST)
+app.listen(port, () => {console.log(`Server is listening on port ${port}`);
+});
 
 
-
-app.listen(port, () => {console.log(`This app is listing to port ${port}`)});
